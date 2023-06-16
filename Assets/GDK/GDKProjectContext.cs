8bb88dbf6bd2a33609b778c09fecbf831574a32d
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-16 12:07:50 +0700

    feat: gdk utils (#18)

---
+++ Assets/GDK/GDKProjectContext.cs
@@ -0,0 +1,21 @@
+﻿namespace GDK
+{
+    using GDK.GDKUtils.Scripts;
+    using GDK.LocalData.Scripts;
+    using GDK.Services.Unity;
+    using Zenject;
+
+    public class GDKProjectContext : MonoInstaller<GDKProjectContext>
+    {
+        public override void InstallBindings()
+        {
+            SignalBusInstaller.Install(this.Container);
+
+            this.Container.InstallDebugLogger();
+
+            LocalDataInstaller.Install(this.Container);
+
+            UnityServiceInstaller.Install(this.Container);
+        }
+    }
+}
---
+++ Assets/GDK/GDKSceneContext.cs
@@ -0,0 +1,17 @@
+﻿namespace GDK
+{
+    using GDK.UIManager.Scripts;
+    using UnityEngine;
+    using Zenject;
+
+    public class GDKSceneContext : MonoInstaller<GDKSceneContext>
+    {
+        [field: SerializeField]
+        private UIManager.Scripts.UIManager UIManager { get; set; }
+
+        public override void InstallBindings()
+        {
+            UIManagerInstaller.Install(this.Container, this.UIManager);
+        }
+    }
+}
