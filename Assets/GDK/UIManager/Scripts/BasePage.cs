01d09dc65059ba06f4d7111b82ee86e93fddc550
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-16 12:07:17 +0700

    feat: UI Manager (#20)
    Co-authored-by: HungHacker102S <HungHacker102S@users.noreply.github.com>

---
+++ Assets/GDK/UIManager/Scripts/BasePage.cs
@@ -0,0 +1,26 @@
+﻿namespace GDK.UIManager.Scripts
+{
+    public abstract class BasePage : BaseScreen
+    {
+        internal override void Show()
+        {
+            this.transform.SetParent(this.UIManager.Page);
+            base.Show();
+        }
+
+        internal override void Hide()
+        {
+            this.transform.SetParent(this.UIManager.Temp);
+
+            foreach (BasePopup popup in this.UIManager.Page) popup.Hide();
+
+            base.Hide();
+        }
+    }
+
+    public abstract class BasePage<TData> : BasePage
+        where TData : class
+    {
+        public new TData Data => base.Data as TData;
+    }
+}
---
+++ Assets/GDK/UIManager/Scripts/UIManagerInstaller.cs
@@ -0,0 +1,23 @@
+﻿namespace GDK.UIManager.Scripts
+{
+    using Zenject;
+
+    public class UIManagerInstaller : Installer<UIManager, UIManagerInstaller>
+    {
+        public UIManagerInstaller(UIManager uiManager)
+        {
+            this.UIManager = uiManager;
+        }
+
+        public UIManager UIManager { get; }
+
+        public override void InstallBindings()
+        {
+            this.Container
+                .Bind<UIManager>()
+                .FromInstance(this.UIManager)
+                .AsCached()
+                .NonLazy();
+        }
+    }
+}
