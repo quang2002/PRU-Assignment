f7efe6f0c6685bfbfaaf3a85bb85d82b4ca7e4f5
dkhai19 <duckhai2002vn@gmail.com>
2023-06-16 11:55:30 +0700

    feat: focus/quit event services

---
+++ Assets/GDK/Services/Unity/UnityServiceInstaller.cs
@@ -0,0 +1,20 @@
+﻿namespace GDK.Services.Unity
+{
+    using Zenject;
+
+    public class UnityServiceInstaller : Installer<UnityServiceInstaller>
+    {
+        public override void InstallBindings()
+        {
+            this.Container.DeclareSignal<ApplicationFocusSignal>();
+            this.Container.DeclareSignal<ApplicationPauseSignal>();
+            this.Container.DeclareSignal<ApplicationQuitSignal>();
+
+            this.Container
+                .Bind<UnityService>()
+                .FromNewComponentOnNewGameObject()
+                .AsCached()
+                .NonLazy();
+        }
+    }
+}
