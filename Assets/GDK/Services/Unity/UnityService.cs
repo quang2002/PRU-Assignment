5182ad009e723aff76a071c04fde3c5d8aba6102
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-16 12:06:51 +0700

    feat: focus/quit event services (#19)
    Co-authored-by: dkhai19 <duckhai2002vn@gmail.com>

---
+++ Assets/GDK/Services/Unity/UnityService.cs
@@ -0,0 +1,35 @@
+﻿namespace GDK.Services.Unity
+{
+    using UnityEngine;
+    using Zenject;
+
+    public class UnityService : MonoBehaviour
+    {
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            this.SignalBus.Fire(new ApplicationFocusSignal { HasFocus = hasFocus });
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            this.SignalBus.Fire(new ApplicationPauseSignal { PauseStatus = pauseStatus });
+        }
+
+        private void OnApplicationQuit()
+        {
+            this.SignalBus.Fire<ApplicationQuitSignal>();
+        }
+
+        #region Inject
+
+        protected SignalBus SignalBus { get; set; }
+
+        [Inject]
+        private void Inject(SignalBus signalBus)
+        {
+            this.SignalBus = signalBus;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/GDK/Services/Unity/UnityServiceSignals.cs
@@ -0,0 +1,16 @@
+﻿namespace GDK.Services.Unity
+{
+    public class ApplicationPauseSignal
+    {
+        public bool PauseStatus { get; internal set; }
+    }
+
+    public class ApplicationFocusSignal
+    {
+        public bool HasFocus { get; internal set; }
+    }
+
+    public class ApplicationQuitSignal
+    {
+    }
+}
