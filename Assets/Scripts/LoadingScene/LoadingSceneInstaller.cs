c4d99babbd346cb50e4167a310de077ae64b1028
HungHacker102S <hackertmh2k2@gmail.com>
2023-06-23 16:14:48 +0700

    feat: loading screen

---
+++ Assets/Scripts/LoadingScene/LoadingSceneInstaller.cs
@@ -0,0 +1,16 @@
+﻿namespace LoadingScene
+{
+    using GDK;
+    using GDK.UIManager.Scripts;
+    using LoadingScene.Screens;
+
+    public class LoadingSceneInstaller : GDKSceneContext
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+
+            this.Container.Resolve<UIManager>().OpenScreen<LoadingScreen>();
+        }
+    }
+}
---
+++ Assets/Scripts/LoadingScene/Screens/LoadingScreen.cs
@@ -0,0 +1,58 @@
+﻿namespace LoadingScene.Screens
+{
+    using System;
+    using GDK.UIManager.Scripts;
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class LoadingScreen : BasePage
+    {
+        #region View
+
+        [field: SerializeField]
+        public Slider SliderProgress { get; set; }
+
+        [field: SerializeField]
+        public TMP_Text TxtProgress { get; set; }
+
+        #endregion
+
+        private float NewProgressValue { get; set; }
+
+        private float CurProgressValue
+        {
+            get => this.SliderProgress.value;
+            set
+            {
+                this.SliderProgress.value = value;
+                this.TxtProgress.text     = $"{value:P}";
+            }
+        }
+
+
+        protected override void OnShow()
+        {
+            base.OnShow();
+            this.NewProgressValue = this.CurProgressValue = 0;
+
+            // TODO: Fake loading, remove it when you will have real loading
+            this.NewProgressValue = 1;
+        }
+
+        private void Update()
+        {
+            this.CurProgressValue = Mathf.Lerp(this.CurProgressValue, this.NewProgressValue, Time.deltaTime);
+
+            if (Math.Abs(this.CurProgressValue - this.NewProgressValue) < 0.01f)
+            {
+                this.CurProgressValue = this.NewProgressValue;
+            }
+
+            if (Math.Abs(this.CurProgressValue - 1) < 0.01f)
+            {
+                this.UIManager.OpenScreen<GameplayScreen>();
+            }
+        }
+    }
+}
