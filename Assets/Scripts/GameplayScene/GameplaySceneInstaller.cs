d3d790c34bf78a498ee7a0b71e06f0d5b24c94c9
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-24 07:25:47 +0000

    feat: gameplay base screen + setting screen (#24)
    * import: UI atlas
    
    * feat: add gameplay screen
    
    * feat: setting screen
    
    ---------
    
    Co-authored-by: CDzung <cdung288@gmail.com>
    Co-authored-by: dkhai19 <duckhai2002vn@gmail.com>

---
+++ Assets/Scripts/GameplayScene/GameplaySceneInstaller.cs
@@ -0,0 +1,16 @@
+﻿namespace GameplayScene
+{
+    using GameplayScene.Screens;
+    using GDK;
+    using GDK.UIManager.Scripts;
+
+    public class GameplaySceneInstaller : GDKSceneContext
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+            
+            this.Container.Resolve<UIManager>().OpenScreen<GameplayScreen>();
+        }
+    }
+}
---
+++ Assets/Scripts/GameplayScene/Screens/GameplayScreen.cs
@@ -0,0 +1,18 @@
+﻿namespace GameplayScene.Screens
+{
+    using GDK.UIManager.Scripts;
+
+    public class GameplayScreen : BasePage
+    {
+        #region View
+
+        // 
+        
+        #endregion
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+    }
+}
