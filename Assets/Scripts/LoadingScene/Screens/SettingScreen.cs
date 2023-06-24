feb0124eb4bf1e63636339abd1ef7143af1b8ea4
dkhai19 <duckhai2002vn@gmail.com>
2023-06-24 14:23:38 +0700

    feat: setting screen

---
+++ Assets/Scripts/LoadingScene/Screens/SettingScreen.cs
@@ -0,0 +1,31 @@
+﻿namespace LoadingScene.Screens
+{
+    using GDK.UIManager.Scripts;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class SettingScreen : BasePopup
+    {
+        #region View
+
+        [field: SerializeField]
+        public Button BtnClose { get; private set; }
+
+        #endregion
+
+        protected override bool IsOverlay => false;
+
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            this.BtnClose.onClick.AddListener(this.OnClickBtnClose);
+        }
+
+        private void OnClickBtnClose()
+        {
+            this.Close();
+        }
+    }
+}
