a21d7dccadd2632fe4fb0b4008d601d7d827d57c
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-24 08:56:37 +0000

    feat: object pool (#26)

---
+++ Assets/GDK/ObjectPool/Scripts/UnityObjectPool.cs
@@ -0,0 +1,47 @@
+namespace GDK.ObjectPool.Scripts
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+
+    public abstract class UnityObjectPool<TObject> : MonoBehaviour, IObjectPool<TObject>
+        where TObject : Object
+    {
+        protected HashSet<TObject> UsedObjects { get; } = new();
+        protected HashSet<TObject> FreeObjects { get; } = new();
+
+        public TObject Instantiate()
+        {
+            var obj = this.FreeObjects.FirstOrDefault();
+
+            if (obj == null)
+            {
+                obj = this.CreateObject();
+            }
+            else
+            {
+                this.FreeObjects.Remove(obj);
+            }
+
+            this.UsedObjects.Add(obj);
+            this.OnInstantiate(obj);
+            return obj;
+        }
+
+        public void Release(TObject obj)
+        {
+            if (this.UsedObjects.Remove(obj))
+            {
+                this.FreeObjects.Add(obj);
+                this.OnRelease(obj);
+                return;
+            }
+
+            Destroy(obj);
+        }
+
+        protected abstract TObject CreateObject();
+        protected abstract void    OnInstantiate(TObject obj);
+        protected abstract void    OnRelease(TObject     obj);
+    }
+}
