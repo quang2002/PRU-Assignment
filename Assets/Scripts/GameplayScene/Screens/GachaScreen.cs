eb5ced51891bad5f8c1faf5b326ebd74d9d61fd2
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-06-30 14:34:35 +0700

    feat: gacha popup (#39)
    Co-authored-by: Oibanoi <banghuund99@gmail.com>

---
+++ Assets/Scripts/GameplayScene/Screens/GachaScreen.cs
@@ -0,0 +1,56 @@
+namespace GameplayScene.Screens
+{
+    using GDK.UIManager.Scripts;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class GachaScreen : BasePopup
+    {
+        #region View
+
+        [field: SerializeField]
+        public Button ButtonX1 { get; private set; }
+
+        [field: SerializeField]
+        public Button ButtonX10 { get; private set; }
+
+        [field: SerializeField]
+        public Transform ItemListTransform { get; private set; }
+
+        #endregion
+
+        protected override bool IsOverlay => false;
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+            this.ButtonX1.onClick.AddListener(this.OnClickGachaX1);
+            this.ButtonX10.onClick.AddListener(this.OnClickGachaX10);
+        }
+
+        protected override void OnShow()
+        {
+            base.OnShow();
+
+            this.ClearAllItems();
+        }
+
+        private void ClearAllItems()
+        {
+            foreach (Transform child in this.ItemListTransform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        private void OnClickGachaX10()
+        {
+                                                                                                                                                                   
+        }
+
+        private void OnClickGachaX1()
+        {
+            
+        }
+    }
+}
