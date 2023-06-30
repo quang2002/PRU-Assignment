27c77b621ed9eeddd4d87e15f5a46c1863e9b022
dkhai19 <duckhai2002vn@gmail.com>
2023-06-30 13:55:01 +0700

    feat: gacha helper class

---
+++ Assets/Scripts/Utilities/GachaHelper.cs
@@ -0,0 +1,56 @@
+namespace Utilities
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GachaHelper<TItem>
+    {
+        public class Entry
+        {
+            public TItem Item   { get; set; }
+            public int   Weight { get; set; }
+
+            public override int GetHashCode()
+            {
+                return this.Item.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Entry entry && this.Item.Equals(entry.Item);
+            }
+        }
+
+        public HashSet<Entry> Entries { get; private set; } = new();
+
+        public void Add(TItem item, int weight)
+        {
+            this.Entries.Add(new Entry { Item = item, Weight = weight });
+        }
+
+        public void Remove(TItem item)
+        {
+            this.Entries.Remove(new Entry { Item = item });
+        }
+
+        public TItem Get()
+        {
+            var totalWeight = this.Entries.Sum(entry => entry.Weight);
+
+            var random = UnityEngine.Random.Range(0, totalWeight);
+
+            var currentWeight = 0;
+            foreach (var entry in this.Entries)
+            {
+                currentWeight += entry.Weight;
+
+                if (random < currentWeight)
+                {
+                    return entry.Item;
+                }
+            }
+
+            return default;
+        }
+    }
+}
