f6dd49cf12aa9f11c53b4c1e77cdfa96bd725e17
dkhai19 <duckhai2002vn@gmail.com>
2023-07-05 11:36:33 +0700

    feat: integrate with gameplay screen

--- Assets/Scripts/GameplayScene/Screens/GameplayScreen.cs
+++ Assets/Scripts/GameplayScene/Screens/GameplayScreen.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Numerics;
+    using GDK.GDKUtils.Scripts;
     using GDK.UIManager.Scripts;
+    using Models.Blueprint;
     using Models.DataControllers;
     using TMPro;
     using UnityEngine;
@@ -51,6 +53,9 @@
         [Inject]
         private MainLocalDataController MainLocalDataController { get; set; }
 
+        [Inject]
+        private UpgradeBlueprint UpgradeBlueprint { get; set; }
+
         #endregion
 
         protected override void OnInit()
@@ -66,6 +71,17 @@
             this.BtnSkill.onClick.AddListener(this.OnClickSkillPopup);
             this.BtnArtifact.onClick.AddListener(this.OnClickArtifactPopup);
             this.BtnShop.onClick.AddListener(this.OnClickShopPopup);
+            this.BtnExit.onClick.AddListener(() => this.QuitApplication());
+        }
+
+        protected override void OnShow()
+        {
+            base.OnShow();
+            this.RebindUpgradeAttack();
+            this.RebindUpgradeHealth();
+            this.RebindUpgradeAttackSpeed();
+            this.RebindUpgradeCriticalRate();
+            this.RebindUpgradeCriticalDamage();
         }
 
         private void OnClickShopPopup()
@@ -88,26 +104,48 @@
             // this.UIManager.OpenScreen<>();
         }
 
-        private void OnClickUpgradeCriticalDamage()
+        private void OnClickUpgradeCriticalDamage() => this.InternalOnClickUpgrade(this.CoinNeededCriticalDamage, this.MainLocalDataController.LevelUpCriticalDamage, this.RebindUpgradeCriticalDamage);
+
+        private void OnClickUpgradeCriticalRate() => this.InternalOnClickUpgrade(this.CoinNeededCriticalRate, this.MainLocalDataController.LevelUpCriticalRate, this.RebindUpgradeCriticalRate);
+
+        private void OnClickUpgradeAttackSpeed() => this.InternalOnClickUpgrade(this.CoinNeededAttackSpeed, this.MainLocalDataController.LevelUpAttackSpeed, this.RebindUpgradeAttackSpeed);
+
+        private void OnClickUpgradeHealth() => this.InternalOnClickUpgrade(this.CoinNeededHealth, this.MainLocalDataController.LevelUpHealth, this.RebindUpgradeHealth);
+
+        private void OnClickUpgradeAttack() => this.InternalOnClickUpgrade(this.CoinNeededAttack, this.MainLocalDataController.LevelUpAttack, this.RebindUpgradeAttack);
+
+        private void InternalOnClickUpgrade(BigInteger coinNeeded, Action levelUpAction, Action rebindAction)
         {
+            var postCoins = this.MainLocalDataController.Coins - coinNeeded;
+
+            if (postCoins < 0) return;
+
+            this.MainLocalDataController.SetCoins(postCoins);
+            levelUpAction.Invoke();
+            rebindAction.Invoke();
         }
 
-        private void OnClickUpgradeCriticalRate()
+        #region Bind UI Values
+
+        private void RebindUpgradeAttack()         => this.InternalRebindUpgrade(this.CoinNeededAttack, this.UpgradeAttack);
+        private void RebindUpgradeHealth()         => this.InternalRebindUpgrade(this.CoinNeededHealth, this.UpgradeHealth);
+        private void RebindUpgradeAttackSpeed()    => this.InternalRebindUpgrade(this.CoinNeededAttackSpeed, this.UpgradeAttackSpeed);
+        private void RebindUpgradeCriticalRate()   => this.InternalRebindUpgrade(this.CoinNeededCriticalRate, this.UpgradeCriticalRate);
+        private void RebindUpgradeCriticalDamage() => this.InternalRebindUpgrade(this.CoinNeededCriticalDamage, this.UpgradeCriticalDamage);
+
+        private void InternalRebindUpgrade(BigInteger coinNeeded, UpgradeComponent upgradeComponent)
         {
+            upgradeComponent.SetCoinNeeded(coinNeeded);
+            upgradeComponent.SetUpgradeButtonState(coinNeeded <= this.MainLocalDataController.Coins);
         }
 
-        private void OnClickUpgradeAttackSpeed()
-        {
-        }
+        public BigInteger CoinNeededAttack         => this.MainLocalDataController.UpgradeData.AttackLevel * this.UpgradeBlueprint.AttackCoinIncrementRate;
+        public BigInteger CoinNeededHealth         => this.MainLocalDataController.UpgradeData.HealthLevel * this.UpgradeBlueprint.HealthCoinIncrementRate;
+        public BigInteger CoinNeededAttackSpeed    => this.MainLocalDataController.UpgradeData.AttackSpeedLevel * this.UpgradeBlueprint.AttackSpeedCoinIncrementRate;
+        public BigInteger CoinNeededCriticalRate   => this.MainLocalDataController.UpgradeData.CriticalRateLevel * this.UpgradeBlueprint.CriticalRateCoinIncrementRate;
+        public BigInteger CoinNeededCriticalDamage => this.MainLocalDataController.UpgradeData.CriticalDamageLevel * this.UpgradeBlueprint.CriticalDamageCoinIncrementRate;
 
-        private void OnClickUpgradeHealth()
-        {
-        }
-
-        private void OnClickUpgradeAttack()
-        {
-            
-        }
+        #endregion
 
         [Serializable]
         public class UpgradeComponent
